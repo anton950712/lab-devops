c0561a7f5710521b5506ed53e099dc444517619a
LAPTOP-J9MV4KFT\anton <anton950712@mail.ru>
2023-04-01 20:27:44 +0200

    fix: update bash script

--- lab_3/shop-angular-cloudfront/local_ci_cd.sh
+++ lab_3/shop-angular-cloudfront/local_ci_cd.sh
@@ -2,8 +2,24 @@
 
 distFolderName=./dist
 
-archiveFilePath=$distFolderName/client-app.zip
+archiveName=client-app.zip
+archiveFilePath=$distFolderName/$archiveName
 appFolderPath=$distFolderName/app
+
+CLIENT_HOST_DIR=$(pwd)/shop-react-redux-cloudfront
+CLIENT_REMOTE_DIR=/var/www
+
+check_remote_dir_exists() {
+  echo "Check if remote directories exist"
+
+  if ssh VM "[ ! -d $1 ]"; then
+    echo "Creating: $1"
+    ssh VM "sudo -S mkdir -p $1 && sudo -S chown -R sshuser: $1"
+  else
+    echo "Clearing: $1"
+    ssh VM "sudo -S rm $1/*"
+  fi
+}
 
 if ! test -f "package.json"
 then
@@ -20,3 +36,12 @@ npm run test
 npm run build
 
 zip -r $archiveFilePath $appFolderPath
+
+check_remote_dir_exists $CLIENT_REMOTE_DIR
+
+echo "---> Building and transfering client files - START <---"
+echo $CLIENT_HOST_DIR
+scp -Cr $archiveFilePath VM:$CLIENT_REMOTE_DIR
+ssh VM "sudo unzip -d $CLIENT_REMOTE_DIR $CLIENT_REMOTE_DIR/$archiveName"
+echo "---> Building and transfering - COMPLETE <---"
+
